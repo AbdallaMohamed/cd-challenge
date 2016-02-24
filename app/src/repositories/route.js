70c516bc649ac57fcca81c573bf92bbbb0d4f62f
Abdalla Mohamed El-Abd <abdalla.mohamed.elabd@gmail.com>
2016-02-24 16:08:18 +0200

    Fixed URLs issue

--- app/src/repositories/route.js
+++ app/src/repositories/route.js
@@ -2,7 +2,7 @@ angular.module('ngChurchDesk').config(function ($stateProvider) {
     $stateProvider
         .state('repositories', {
             url: '/Repositories?user',
-            templateUrl: 'src/repositories/views/repositories.partial.html',
+            templateUrl: './src/repositories/views/repositories.partial.html',
             controller: 'repositoriesCtrl',
             title: 'Repositories'
         });
--- app/src/users/route.js
+++ app/src/users/route.js
@@ -2,7 +2,7 @@ angular.module('ngChurchDesk').config(function ($stateProvider) {
     $stateProvider
         .state('users', {
             url: '/Users',
-            templateUrl: 'src/users/views/users.partial.html',
+            templateUrl: './src/users/views/users.partial.html',
             controller: 'usersCtrl',
             title: 'Users'
         });
